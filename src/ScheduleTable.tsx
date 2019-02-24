a4998b846c94e63e9d92be4df8707596e3060e00
Takanori Hoshi <takanori.hoshi@gmail.com>
2019-02-23 22:02:33 -0800

    Display deleted values in deleted style

--- src/ScheduleTable.tsx
+++ src/ScheduleTable.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import { Exam, CourseExam, makeCourseExams } from './Schedule';
 
+const scheduleTableSchema = [
+  { name: 'SECTION', key: 'section' },
+  { name: 'INSTRUCTOR', key: 'instructor' },
+  { name: 'DATE', key: 'date' },
+  { name: 'TIME', key: 'start_time', key2: 'end_time' },
+  { name: 'BLDG', key: 'building' },
+  { name: 'ROOM', key: 'room' }
+];
+
 const ShceduleTableHead = () => {
   return (
     <thead>
       <tr>
         <th>COURSE</th>
-        <th>SECTION</th>
-        <th>INSTRUCTOR</th>
-        <th>DATE</th>
-        <th>TIME</th>
-        <th>BLDG</th>
-        <th>ROOM</th>
+        {scheduleTableSchema.map(each => <th key={each.key}>{each.name}</th>)}
       </tr>
     </thead>
   );
 };
+
+const shceduleTableCell = (exam: any, key: string, key2: string | undefined) => {
+  const del = exam['del_' + key];
+  if (key2) {
+    const del2 = exam['del_' + key2];
+    return <td key={key}>{del && del2 && <><del>{del} - {del2}</del><br /></>}{exam[key]} - {exam[key2]}</td>;
+  } else {
+    return <td key={key}>{del && <><del>{del}</del><br /></>}{exam[key]}</td>;
+  }
+}
 
 const ShceduleTableBody = (props: { courseExams: CourseExam[] }) => {
   return (
@@ -23,13 +37,8 @@ const ShceduleTableBody = (props: { courseExams: CourseExam[] }) => {
       {props.courseExams.flatMap(courseExam =>
         courseExam.exams.map((exam, index) =>
           <tr key={exam.course + '-' + exam.section}>
-            {index == 0 && <td rowSpan={courseExam.exams.length}>{exam.course}</td>}
-            <td>{exam.section}</td>
-            <td>{exam.instructor}</td>
-            <td>{exam.date}</td>
-            <td>{exam.start_time} - {exam.end_time}</td>
-            <td>{exam.building}</td>
-            <td>{exam.room}</td>
+            {index === 0 && <td rowSpan={courseExam.exams.length}>{exam.course}</td>}
+            {scheduleTableSchema.map(each => shceduleTableCell(exam, each.key, each.key2))}
           </tr>
         )
       )}
